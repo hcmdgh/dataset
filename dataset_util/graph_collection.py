206d8164267a9dd5f0b45eab4f15431411dc9c1a
Hao Geng <hcmdgh@qq.com>
2025-12-29 21:15:54 +0800

    update

--- dataset_util/graph_collection.py
+++ dataset_util/graph_collection.py
@@ -98,9 +98,28 @@ def load_exp_dataset(
     return graph_batch
 
 
+def load_cluster_dataset(
+    add_self_loop: bool,
+) -> tuple[Batch, Batch, Batch]:
+    dataset_dir = os.path.join(dataset_root, 'GNN-Benchmark-Dataset/CLUSTER')
+
+    with open(os.path.join(dataset_dir, f'processed_data/train_val_test_graph_batch.pkl'), 'rb') as r:
+        graph_batch_list = pickle.load(r)
+
+    graph_batch_list = [
+        transform_graph_batch(
+            graph_batch = graph_batch,
+            add_node_feat = 'none',
+            add_self_loop = add_self_loop,
+        )
+        for graph_batch in graph_batch_list
+    ]
+
+    return tuple(graph_batch_list)
+
+
 if __name__ == '__main__':
-    data = load_exp_dataset(
-        add_node_feat = 'randn_128',
+    data = load_cluster_dataset(
         add_self_loop = True,
     )
     print(data)
